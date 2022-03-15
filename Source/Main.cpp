3f232d4f22f4868a19771f55f2260bd8a0e42598
cgbsu <cgreeley@student.bridgew.edu>
2022-03-15 22:32:30 +0000

    Parsed a parenthesisgit add *

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -24,6 +24,7 @@ constexpr ctpg::regex_term< natural_number_regex > natural_number_term( "Natural
 
 constexpr ctpg::nterm< size_t > factor( "Factor" );
 constexpr ctpg::nterm< size_t > sum( "Sum" );
+constexpr ctpg::nterm< size_t > parenthesis_scope( "ParenthesisScope" );
 
 
 // This function was "yoiked" directly from https://github.com/peter-winter/ctpg //
@@ -39,46 +40,64 @@ constexpr ctpg::char_term plus_term( '+', 1, ctpg::associativity::ltor );
 constexpr ctpg::char_term minus_term( '-', 1, ctpg::associativity::ltor );
 constexpr ctpg::char_term multiply_term( '*', 2, ctpg::associativity::ltor );
 constexpr ctpg::char_term divide_term( '/', 2, ctpg::associativity::ltor );
+constexpr ctpg::char_term left_parenthesis_term( '(', 3, ctpg::associativity::ltor );
+constexpr ctpg::char_term right_parenthesis_term( ')', 3, ctpg::associativity::ltor );
 
 
 
 constexpr ctpg::parser factor_parser( 
         factor, 
-        ctpg::terms( multiply_term, divide_term, plus_term, minus_term, natural_number_term ), 
-        ctpg::nterms( factor, sum ), 
-        ctpg::rules(
+        ctpg::terms( 
+                multiply_term, 
+                divide_term, 
+                plus_term, 
+                minus_term, 
+                natural_number_term, 
+                left_parenthesis_term, 
+                right_parenthesis_term 
+            ), 
+        ctpg::nterms( factor, sum, parenthesis_scope ), 
+        ctpg::rules( 
                 factor( natural_number_term ) >= to_size_t, 
-                // sum( natural_number_term ) >= to_size_t, 
                 factor( factor, multiply_term, natural_number_term ) 
-                        >= []( size_t current_factor, auto factor_operator, const auto& next_token ) { // This lambda also yoikned. //
+                        >= []( size_t current_factor, auto, const auto& next_token ) {
                                 return current_factor * to_size_t( next_token ); 
                             }, 
                 factor( factor, divide_term, natural_number_term ) 
-                        >= []( size_t current_factor, auto factor_operator, const auto& next_token ) { // This lambda also yoikned. //
+                        >= []( size_t current_factor, auto, const auto& next_token ) {
                                 return current_factor / to_size_t( next_token ); 
                             },
+                parenthesis_scope( left_parenthesis_term, factor, right_parenthesis_term )
+                        >= [] ( auto, auto factor, auto ) { return factor; }, 
+                factor( parenthesis_scope ) >= []( auto parenthesis_scope ) { return parenthesis_scope; }, 
                 factor( sum ) >= []( auto sum ) { return sum; }, 
                 sum( factor, plus_term, factor ) 
-                        >= []( auto current_sum, auto sum_operator, const auto& next_token ) {
+                        >= []( auto current_sum, auto, const auto& next_token ) {
                                 return  current_sum + next_token; 
                             }, 
                 sum( factor, minus_term, factor ) 
-                        >= []( auto current_sum, auto sum_operator, const auto& next_token ) {
+                        >= []( auto current_sum, auto, const auto& next_token ) {
                                 return current_sum - next_token; 
-                            } 
+                            }
            )
 );
 
 int main( int argc, char** args )
 {
-
-    auto parse_result = factor_parser.parse( 
-            ctpg::parse_options{}.set_verbose(), 
-            ctpg::buffers::string_buffer( args[ 1 ] ), std::cerr 
-        );
-    if( parse_result.has_value() == true )
-        std::cout << parse_result.value() << "\n";
-    else
-        std::cerr << "Error failed to parse!\n";
+    std::cout << "Hello world! Please enter your expression: ";
+    for( std::string input = "4*10+2"; input != "end"; std::getline( std::cin, input ) )
+    {
+        std::cout << "Got: " << input << "\n";
+        if( auto parse_result = factor_parser.parse( 
+                            //ctpg::parse_options{}.set_verbose(), 
+                            ctpg::buffers::string_buffer( input.c_str() ), std::cerr 
+                    ); 
+                    parse_result.has_value() == true 
+                )
+            std::cout << "Result: " << parse_result.value() << "\n";
+        else
+            std::cerr << "Error failed to parse!\n";
+        std::cout << "Enter prompt please: ";
+    }
     return 0;
 }
