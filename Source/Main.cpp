dc97f7fe5bd4cb3e086d7132ba0d1cd8b3114d77
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-04-07 15:47:03 +0000

    This was a good idea, sadly not to be.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -8,17 +8,38 @@
 constexpr char star_token = '*';
 constexpr char forward_slash_token = '/';
 constexpr char plus_token = '+';
-constexpr char minus_token = '+';
+constexpr char minus_token = '-';
 constexpr char caret_token = '^';
 constexpr char colon_token = ':';
 
-enum class ExpressionOperators : char 
+constexpr char open_parenthesis = '(';
+constexpr char close_parenthesis = ')';
+constexpr char open_curly_bracket = '{';
+constexpr char close_curly_bracket = '}';
+constexpr char open_square_bracket = '[';
+constexpr char close_square_bracket = ']';
+constexpr char open_angle_bracket = '<';
+constexpr char close_angle_bracket = '>';
+
+enum class ExpressionOperator : char 
 {
     FactorMultiply = star_token, 
     FactorDivide = forward_slash_token, 
     SumAdd = plus_token, 
     SumSubtract = minus_token, 
     ExponentRaise = caret_token 
+};
+
+enum class ScopeOperators : char 
+{
+    OpenParenthesis = open_parenthesis, 
+    CloseParenthesis = close_parenthesis, 
+    OpenCurlyBracket = open_curly_bracket, 
+    CloseCurlyBracket = close_curly_bracket, 
+    OpenSquareBracket = open_square_bracket, 
+    CloseSquareBracket = close_square_bracket, 
+    OpenAngleBracket = open_angle_bracket, 
+    CloseAngleBracket = close_angle_bracket 
 };
 
 enum class BooleanOperators : size_t
@@ -32,8 +53,13 @@ enum class BooleanOperators : size_t
     Or = 6 
 };
 
+enum class OtherNodeType : size_t
+{
+    FactorStem = 0 
+};
+
 // template< typename CanidateType >
-// concept ArithmaticConcept = requires( const CanidateType canidate )
+// concept typename /*ArithmaticConcept*/ = requires( const CanidateType canidate )
 // {
 //     { canidate * canidate } -> std::convertible_to< CanidateType >;
 //     { canidate / canidate } -> std::convertible_to< CanidateType >;
@@ -42,178 +68,192 @@ enum class BooleanOperators : size_t
 // };
 
 template< typename... ArithmaticParameterTypes >
-struct LiteralFactor
+struct StrongFactor
 {
     using FactorType = std::variant< ArithmaticParameterTypes... >;
-    using ThisType = LiteralFactor< ArithmaticParameterTypes... >;
+    using ThisType = StrongFactor< ArithmaticParameterTypes... >;
     const FactorType factor;
     
-    explicit constexpr LiteralFactor( FactorType factor ) : factor( factor ) {}
+    StrongFactor() = default;
+    explicit constexpr StrongFactor( FactorType factor ) : factor( factor ) {}
+    constexpr StrongFactor( const ThisType& other ) = default;
+    constexpr StrongFactor( ThisType&& other ) = default;
 
     constexpr ThisType operator*( const auto other ) {
         return ThisType{ *std::get_if< decltype( other ) >( &factor ) * other };
     }
     constexpr ThisType operator/( const auto other ) {
-        return ThisType{ *std::get_if< decltype( other ) >( &factor ) * other };
+        return ThisType{ *std::get_if< decltype( other ) >( &factor ) / other };
     }
-    constexpr ThisType operator_( const auto other ) {
-        return ThisType{ *std::get_if< decltype( other ) >( &factor ) * other };
+    constexpr ThisType operator+( const auto other ) {
+        return ThisType{ *std::get_if< decltype( other ) >( &factor ) + other };
     }
     constexpr ThisType operator-( const auto other ) {
-        return ThisType{ *std::get_if< decltype( other ) >( &factor ) * other };
+        return ThisType{ *std::get_if< decltype( other ) >( &factor ) - other };
     }
     constexpr bool operator<=>( const auto other ) {
         return factor.operator<=>( other );
     }
 };
 
-using FactorType = LiteralFactor< size_t, int, float, double >;
 
-template< typename, auto, typename... >
+template< typename... ArithmaticParameterTypes >
+struct WeakFactor
+{
+    using FactorType = std::variant< ArithmaticParameterTypes... >;
+    using ThisType = WeakFactor< ArithmaticParameterTypes... >;
+    const FactorType factor;
+    
+    WeakFactor() = default;
+    explicit constexpr WeakFactor( FactorType factor ) : factor( factor ) {}
+    constexpr WeakFactor( const ThisType& other ) = default;
+    constexpr WeakFactor( ThisType&& other ) = default;
+
+    constexpr ThisType operator*( const auto other ) {
+        return ThisType{ std::visit( [ & ]( auto value ) { return value * other; }, factor ) };
+    }
+    constexpr ThisType operator/( const auto other ) {
+        return ThisType{ std::visit( [ & ]( auto value ) { return value / other; }, factor ) };
+    }
+    constexpr ThisType operator+( const auto other ) {
+        return ThisType{ std::visit( [ & ]( auto value ) { return value + other; }, factor ) };
+    }
+    constexpr ThisType operator-( const auto other ) {
+        return ThisType{ std::visit( [ & ]( auto value ) { return value - other; }, factor ) };
+    }
+    constexpr bool operator<=>( const auto other ) {
+        return factor.operator<=>( other );
+    }
+};
+
+using FactorType = StrongFactor< size_t, int, float, double >;
+
+template< auto, auto... >
 struct ExpressionNode
 {
+    using VariantType = void;
+    using ThisType = void;
     static_assert( "Error::Attempt to instantiate unconstrained expression" );
 };
 
 template<>
-struct ExpressionNode< FactorType, nullptr > : public FactorType
+struct ExpressionNode< OtherNodeType::FactorStem, OtherNodeType::FactorStem >
 {
+    FactorType literal;
+    using VariantType = std::variant< ExpressionNode< OtherNodeType::FactorStem, OtherNodeType::FactorStem > >;
+    using ThisType = ExpressionNode< OtherNodeType::FactorStem, OtherNodeType::FactorStem >;
+    // ExpressionNode() = default;
+    explicit ExpressionNode( const FactorType literal ) : literal( literal ) {}
+    // ExpressionNode( const ThisType& other ) = default;
+    // ExpressionNode( ThisType&& other ) = default;
 
-};
-
-template< 
-        ArithmaticConcept LeftParameterType, 
-        ExpressionOperators OperationParameterConstant,  
-        ArithmaticConcept RightParameterType 
-    >
-struct ExpressionNode< LeftParameterType, OperationParameterConstant, RightParameterType >
-{
-    constexpr static const ExpressionOperators operation = OperationParameterConstant;
-    using LeftType = LeftParameterType;
-    using RightType = RightParameterType;
-    const LeftType left;
-    const RightType right;
-    using ThisType = ExpressionNode< LeftType, OperationParameterConstant, RightType >;
-
-    ExpressionNode( const LeftType left, const RightType right ) 
-            : left( left ), right( right ) {}
 
     constexpr auto operator*( auto other )
     {
         return ExpressionNode< 
-                ThisType, 
-                ExpressionOperators::FactorMultiply, 
-                decltype( other ) 
+                ExpressionOperator::FactorMultiply 
+            >{ ThisType{ literal }, other };
+    }
+    constexpr auto operator/( const auto& other )
+    {
+        return ExpressionNode< 
+                ExpressionOperator::FactorDivide 
+            >{ ThisType{ literal }, other };
+    }
+    constexpr auto operator+( const auto& other )
+    {
+        return ExpressionNode< 
+                ExpressionOperator::SumAdd 
+            >{ ThisType{ literal }, other };
+    }
+    constexpr auto operator*( const auto& other )
+    {
+        return ExpressionNode< 
+                ExpressionOperator::SumSubtract 
+            >{ ThisType{ literal }, other };
+    }
+};
+
+template< ExpressionOperator OperationParameterConstant >
+struct ExpressionNode< 
+        OperationParameterConstant, 
+        OtherNodeType::FactorStem, 
+        ExpressionOperator::FactorMultiply, 
+        ExpressionOperator::FactorDivide, 
+        ExpressionOperator::SumAdd, 
+        ExpressionOperator::SumSubtract 
+    >
+{
+    constexpr static const ExpressionOperator operation = OperationParameterConstant;
+    template< auto SubOperationParameterConstant >
+    using VariantAlternativeType = ExpressionNode< 
+            SubOperationParameterConstant, 
+            OtherNodeType::FactorStem, 
+            ExpressionOperator::FactorMultiply, 
+            ExpressionOperator::FactorDivide, 
+            ExpressionOperator::SumAdd, 
+            ExpressionOperator::SumSubtract 
+        >;
+    using VariantType = std::variant< 
+            VariantAlternativeType< OtherNodeType::FactorStem >, 
+            VariantAlternativeType< ExpressionOperator::FactorMultiply >, 
+            VariantAlternativeType< ExpressionOperator::FactorDivide >, 
+            VariantAlternativeType< ExpressionOperator::SumAdd >, 
+            VariantAlternativeType< ExpressionOperator::SumSubtract > 
+        >;
+    const VariantType left;
+    const VariantType right;
+    using ThisType = VariantAlternativeType< OperationParameterConstant >;
+
+    // ExpressionNode() = default;
+    explicit ExpressionNode( 
+                    const VariantType left, 
+                    const VariantType right 
+            ) : left( left ), right( right ) {}
+    // ExpressionNode( const ThisType& other ) = default;
+    // ExpressionNode( ThisType&& other ) = default;
+
+    constexpr auto operator*( auto other )
+    {
+        return ExpressionNode< 
+                ExpressionOperator::FactorMultiply 
             >{ ThisType{ left, right }, other };
     }
     constexpr auto operator/( const auto& other )
     {
         return ExpressionNode< 
-                ThisType, 
-                ExpressionOperators::FactorDivide, 
-                decltype( other ) 
+                ExpressionOperator::FactorDivide 
             >{ ThisType{ left, right }, other };
     }
     constexpr auto operator+( const auto& other )
     {
         return ExpressionNode< 
-                ThisType, 
-                ExpressionOperators::SumAdd, 
-                decltype( other ) 
+                ExpressionOperator::SumAdd 
             >{ ThisType{ left, right }, other };
     }
     constexpr auto operator*( const auto& other )
     {
         return ExpressionNode< 
-                ThisType, 
-                ExpressionOperators::SumSubtract, 
-                decltype( other ) 
+                ExpressionOperator::SumSubtract 
             >{ ThisType{ left, right }, other };
     }
 };
 
-template< 
-        ArithmaticConcept LeftParameterType, 
-        BooleanOperators OperationParameterConstant,  
-        ArithmaticConcept RightParameterType 
-    >
-struct ExpressionNode< LeftParameterType, OperationParameterConstant, RightParameterType >
-{
-    constexpr static const ExpressionOperators operation = OperationParameterConstant;
-    using LeftType = LeftParameterType;
-    using RightType = RightParameterType;
-    const LeftType left;
-    const RightType right;
-    using ThisType = ExpressionNode< LeftType, OperationParameterConstant, RightType >;
+using ArithmaticVaraintType = WeakFactor< 
+        ExpressionNode< OtherNodeType::FactorStem >, 
+        ExpressionNode< ExpressionOperator::FactorMultiply >, 
+        ExpressionNode< ExpressionOperator::FactorDivide >, 
+        ExpressionNode< ExpressionOperator::SumAdd >, 
+        ExpressionNode< ExpressionOperator::SumSubtract > 
+    >;
 
-    ExpressionNode( const LeftType left, const RightType right ) 
-            : left( left ), right( right ) {}
-
-    constexpr auto operator==( auto other )
-    {
-        return ExpressionNode< 
-                ThisType, 
-                BooleanOperators::EqualTo, 
-                decltype( other ) 
-            >{ ThisType{ left, right }, other };
-    }
-    constexpr auto operator<( const auto& other )
-    {
-        return ExpressionNode< 
-                ThisType, 
-                BooleanOperators::LessThan, 
-                decltype( other ) 
-            >{ ThisType{ left, right }, other };
-    }
-    constexpr auto operator>( const auto& other )
-    {
-        return ExpressionNode< 
-                ThisType, 
-                BooleanOperators::GreaterThan, 
-                decltype( other ) 
-            >{ ThisType{ left, right }, other };
-    }
-    constexpr auto operator>=( const auto& other )
-    {
-        return ExpressionNode< 
-                ThisType, 
-                BooleanOperators::GreaterThanOrEqualTo, 
-                decltype( other ) 
-            >{ ThisType{ left, right }, other };
-    }
-    constexpr auto operator<=( const auto& other )
-    {
-        return ExpressionNode< 
-                ThisType, 
-                BooleanOperators::LessThanOrEqualTo, 
-                decltype( other ) 
-            >{ ThisType{ left, right }, other };
-    }
-    constexpr auto operator&&( const auto& other )
-    {
-        return ExpressionNode< 
-                ThisType, 
-                BooleanOperators::And, 
-                decltype( other ) 
-            >{ ThisType{ left, right }, other };
-    }
-    constexpr auto operator||( const auto& other )
-    {
-        return ExpressionNode< 
-                ThisType, 
-                BooleanOperators::Or, 
-                decltype( other ) 
-            >{ ThisType{ left, right }, other };
-    }
-};
 
 constexpr char natural_number_regex[] = "[0-9][0-9]*";
 constexpr ctpg::regex_term< natural_number_regex > natural_number_term( "NaturalNumber" );
 
-constexpr ctpg::nterm< FactorType > factor( "Factor" );
-constexpr ctpg::nterm< FactorType > sum( "Sum" );
-constexpr ctpg::nterm< FactorType > parenthesis_scope( "ParenthesisScope" );
-constexpr ctpg::
+constexpr ctpg::nterm< ArithmaticVaraintType > factor( "Factor" );
+constexpr ctpg::nterm< ArithmaticVaraintType > sum( "Sum" );
+constexpr ctpg::nterm< ArithmaticVaraintType > parenthesis_scope( "ParenthesisScope" );
 
 // This function was "yoiked" directly from https://github.com/peter-winter/ctpg //
 constexpr size_t to_size_t( std::string_view integer_token )
@@ -224,20 +264,24 @@ constexpr size_t to_size_t( std::string_view integer_token )
     return sum;
 }
 
+constexpr char char_cast( ExpressionOperator operation ) {
+    return static_cast< char >( operation );
+}
+
 constexpr ctpg::char_term plus_term( 
-        static_cast< char >( ExpressionOperators::SumAdd ), 
-        static_cast< size_t >( 1 ) , ctpg::associativity::ltor );
+        char_cast( ExpressionOperator::SumAdd ), 1, ctpg::associativity::ltor 
+    );
 constexpr ctpg::char_term minus_term( 
-        static_cast< char >( ExpressionOperators::SumSubtract ), 
-        static_cast< size_t >( 1 ), ctpg::associativity::ltor );
+        char_cast( ExpressionOperator::SumSubtract ), 1, ctpg::associativity::ltor 
+    );
 constexpr ctpg::char_term multiply_term( 
-        static_cast< char >( ExpressionOperators::FactorMultiply ), 
-        static_cast< size_t >( 2 ), ctpg::associativity::ltor );
+        char_cast( ExpressionOperator::FactorMultiply ), 2, ctpg::associativity::ltor 
+    );
 constexpr ctpg::char_term divide_term( 
-        static_cast< char >( ExpressionOperators::FactorDivide ), 
-        static_cast< size_t >( 2 ), ctpg::associativity::ltor );
-constexpr ctpg::char_term left_parenthesis_term( '(', static_cast< size_t >( 3 ), ctpg::associativity::ltor );
-constexpr ctpg::char_term right_parenthesis_term( ')', static_cast< size_t >( 3 ), ctpg::associativity::ltor );
+        char_cast( ExpressionOperator::FactorDivide ), 2, ctpg::associativity::ltor 
+    );
+constexpr ctpg::char_term left_parenthesis_term( '(', 3, ctpg::associativity::ltor );
+constexpr ctpg::char_term right_parenthesis_term( ')', 3, ctpg::associativity::ltor );
 
 
 constexpr ctpg::parser factor_parser( 
@@ -253,10 +297,15 @@ constexpr ctpg::parser factor_parser(
             ), 
         ctpg::nterms( factor, sum, parenthesis_scope ), 
         ctpg::rules( 
-                factor( natural_number_term ) >= to_size_t, 
+                factor( natural_number_term ) 
+                        >= []( auto token ) {
+                            return ExpressionNode< OtherNodeType::FactorStem, OtherNodeType::FactorStem >{ 
+                                    FactorType{ to_size_t( token ) }
+                                };
+                            }, 
                 factor( factor, multiply_term, natural_number_term ) 
                         >= []( auto current_factor, auto, const auto& next_token ) {
-                                return LiteralFactor{ current_factor * to_size_t( next_token ) }; 
+                                return current_factor * to_size_t( next_token ); 
                             }, 
                 factor( factor, divide_term, natural_number_term ) 
                         >= []( auto current_factor, auto, const auto& next_token ) {
