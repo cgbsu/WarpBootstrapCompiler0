93a12e592167a06b1fe606d0ea881e2709434aa0
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-05-04 18:45:10 +0000

    For some reason products are crashing

--- Include/Warp/ExpressionAnalysis.hpp
+++ Include/Warp/ExpressionAnalysis.hpp
@@ -15,17 +15,29 @@ namespace Warp::Analysis
     {
         const auto& node = to_const_reference( variant );
         return Warp::Utilities::visit< []( auto raw_node_pointer ) { 
-                return FeedbackParameterConstant( *raw_node_pointer ); 
+            std::cout << Warp::Utilities::type_name< decltype( *raw_node_pointer ) >();
+                const decltype( *raw_node_pointer )& ref = *raw_node_pointer;
+                return FeedbackParameterConstant( ref ); 
             }>( node );
     }
 
     template< typename ExpressionParameterType >
-    auto compute_value_of_expression( const auto& node )
+    auto compute_value_of_expression( const Warp::AbstractSyntaxTree::Node< Warp::AbstractSyntaxTree::NodeType::Literal >& node )
     {
-        const auto value_from = []( auto from ) { 
+        // return Warp::Utilities::visit< [ & ]( const auto& value_pointer ) { 
+        //         return *value_pointer; //is_of_type< ExpressionParameterType >( *value_pointer ); 
+        //     } >( to_const_reference( node.value.factor ) );
+        return 0;
+    }
+
+    template< typename ExpressionParameterType, Warp::Parser::ExpressionOperator OperatorParameterConstant >
+    auto compute_value_of_expression( const Warp::AbstractSyntaxTree::Node< OperatorParameterConstant >& node )
+    {
+        const auto value_from = []( const Warp::AbstractSyntaxTree::VariantType& from ) { 
                 return abstract_syntax_tree_callback< compute_value_of_expression< ExpressionParameterType > >( from ); 
             };
-        return decltype( node )::operation( 
+        using OperationNodeType = std::decay_t< Warp::Utilities::CleanType< decltype( node ) > >;
+        return OperationNodeType::operation( 
                 value_from( node.left ), 
                 value_from( node.right ) 
             );
@@ -35,19 +47,12 @@ namespace Warp::Analysis
     auto is_of_type( auto canidate )
     {
         static_assert( 
-                std::is_same< QueryParameterType, decltype( canidate ) >::value, 
+                std::is_same< std::decay_t< QueryParameterType >, std::decay_t< decltype( canidate ) > >::value, 
                 "Error:: Attempt to parse literal in expression with a type that differed than the parsed literal type" 
             );
         return canidate;
     }
 
-    template< typename ExpressionParameterType >
-    auto compute_value_of_expression( const Warp::AbstractSyntaxTree::Node< Warp::AbstractSyntaxTree::NodeType::Literal >& node )
-    {
-        return Warp::Utilities::visit< [ & ]( auto value_pointer ) { 
-                return is_of_type< ExpressionParameterType >( *value_pointer ); 
-            } >( to_const_reference( node.value.factor ) );
-    }
 }
 
 #endif // WARP_BOOTSTRAP_COMPILER_HEADER_UTILITIES_HPP
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -124,42 +124,6 @@ constexpr ctpg::parser factor_parser(
            )
 );
 
-// https://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c
-#include <type_traits>
-#include <typeinfo>
-#ifndef _MSC_VER
-#   include <cxxabi.h>
-#endif
-#include <memory>
-#include <string>
-#include <cstdlib>
-
-template <class T>
-std::string type_name()
-{
-    typedef typename std::remove_reference<T>::type TR;
-    std::unique_ptr<char, void(*)(void*)> own
-           (
-#ifndef _MSC_VER
-                abi::__cxa_demangle(typeid(TR).name(), nullptr,
-                                           nullptr, nullptr),
-#else
-                nullptr,
-#endif
-                std::free
-           );
-    std::string r = own != nullptr ? own.get() : typeid(TR).name();
-    if (std::is_const<TR>::value)
-        r += " const";
-    if (std::is_volatile<TR>::value)
-        r += " volatile";
-    if (std::is_lvalue_reference<T>::value)
-        r += "&";
-    else if (std::is_rvalue_reference<T>::value)
-        r += "&&";
-    return r;
-}
-
 int main( int argc, char** args )
 {
     std::cout << "Hello world! Please enter your expression: ";
@@ -174,6 +138,7 @@ int main( int argc, char** args )
                 )
         {
             const auto& result = parse_result.value();
+            Warp::Analysis::abstract_syntax_tree_callback< Warp::Analysis::compute_value_of_expression< size_t > >( result );
         }
         else
             std::cerr << "Error failed to parse!\n";
