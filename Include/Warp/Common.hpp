f829aa7fdf8e268ce8c6482eef63db64e8367ba9
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-05-06 04:18:39 +0000

    :eyes:

--- Include/Warp/Common.hpp
+++ Include/Warp/Common.hpp
@@ -7,3 +7,9 @@
 #include <any>
 
 #include <ctpg.hpp>
+
+// Quote from https://github.com/Neargye/magic_enum/blob/master/doc/limitations.md
+// This library uses a compiler-specific hack (based on __PRETTY_FUNCTION__ / __FUNCSIG__), which works on Clang >= 5, MSVC >= 15.3 and GCC >= 9
+// Lets bank on reflection coming in C++26... even though I have been waiting for it since C++03/I was 14 
+
+// #include <magic_enum.hpp>
